c7abc5f08535cd15bf798a37ed2088b996f15419
rushitrivedi83 <rushitrivedi83@gmail.com>
2022-05-02 07:20:29 -0400

    User Purchase History

--- partials/nav.php
+++ partials/nav.php
@@ -40,6 +40,7 @@ session_start();
                 <li class="nav-item"><a class="nav-link" href="<?php echo get_url('shop.php'); ?>">Shop</a></li>
                 <?php if (is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('purchase_history.php'); ?>">Purchase History</a></li>
                 <?php endif; ?>
                 <?php if (!is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('login.php'); ?>">Login</a></li>
