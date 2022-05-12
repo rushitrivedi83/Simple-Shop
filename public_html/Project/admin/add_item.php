4af8af87b5ba8d4761a67b1722bc8288914c8cf0
rushitrivedi83 <rushitrivedi83@gmail.com>
2022-05-12 10:51:47 -0400

    Fixing Create Items Bug

--- public_html/Project/admin/add_item.php
+++ public_html/Project/admin/add_item.php
@@ -15,7 +15,7 @@ if (isset($_POST["submit"])) {
 //get the table definition
 $columns = get_columns($TABLE_NAME);
 //echo "<pre>" . var_export($columns, true) . "</pre>";
-$ignore = ["id", "modified", "created"];
+$ignore = ["id", "modified", "created", "avg_rating"];
 ?>
 
 <script>
